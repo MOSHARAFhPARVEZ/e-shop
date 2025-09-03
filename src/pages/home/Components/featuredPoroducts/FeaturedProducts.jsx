8b410c1448e90a7c1a1c7f7d367872a7e9c422d6
Md Mosharaf Hossain <parveznbh2026@gmail.com>
2025-09-03 23:21:39 +0600

    part-11

--- src/pages/home/Components/featuredPoroducts/FeaturedProducts.jsx
+++ src/pages/home/Components/featuredPoroducts/FeaturedProducts.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { FaArrowRightLong } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
 
 const FeaturedProducts = () => {
+  const {t} = useTranslation()
   return (
-    <div>
-        
-    </div>
+    <>
+      <div className='mt-[80px] flex items-center justify-between'>
+        <h2 className='font-poppins font-semibold text-[36px] text-black01 leading-[46px]'>{t('Featured_Products')}</h2>
+        <div className='flex items-center gap-x-4 text-orange group'>
+          <Link to='/product'className='font-bold text-base font-montserrat leading-[24px] '>
+            <span className='group-hover:text-black01'>{t('View_All')}</span>
+          </Link>
+          <span className='text-orange group-hover:text-black01 group-hover:translate-x-2 transition-transform duration-300'><FaArrowRightLong size='28px'/></span>
+        </div>
+      </div>
+
+    </>
   )
 }
 
---
+++ src/pages/home/Components/featuredPoroducts/featuredData.js
@@ -0,0 +1,11 @@
+import ImageOne from '../../../../assets/ProductImage/imageOne.webp'
+
+export const FeaturedProductData =[
+    {
+        image: ImageOne,
+        category: "PHONE",
+        title: "JPhone 13 High Quality Value Buy Best Camera",
+        rating: 5,
+        price: 999.00
+    }
+] 
