16bc4d91bbf1c782d8b58a9a638023d847881f62
Md Mosharaf Hossain <parveznbh2026@gmail.com>
2025-09-05 18:45:05 +0600

    part-18

--- src/pages/home/Components/NewProduct/NewProduct.jsx
+++ src/pages/home/Components/NewProduct/NewProduct.jsx
@@ -1,53 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import ProductCards from '../../../../globalComponents/productCards/ProductCard'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper/modules'
-import { FeaturedProductData } from '../featuredPoroducts/featuredData'
 import MenubarCategories from '../../../../globalComponents/headerComponents/menubar/allcategories/MenubarCategories'
+import { NewProductData } from './NewProductData'
 
 
 const NewProduct = () => {
+    const [initialData,setinitialData] = useState(5);
     const {t} = useTranslation()
-  return (
-    <>
-        <div className='relative mt-[80px]'>
-        <div className='flex items-center justify-between mb-[48px]'>
-          <h2 className='font-poppins font-semibold text-[36px] text-black01 leading-[46px]'>{t('New_Products')}</h2>
-          <div className='flex items-end gap-x-4'>
-            <p className='font-montserrat font-normal text-base leading-6 text-black01'>{t('Sort_by')}</p>
-            <MenubarCategories icons={true} className='font-montserrat font-bold text-base text-orange leading-[24px] cursor-pointer'/>
-            
-          </div>
-        </div>
-        {/* <div className='absolute top-2/4 -translate-y-2/4 -right-5 z-30 swiper-next-btn'><NextBtn/></div>
-        <div className='absolute top-2/4 -translate-y-2/4 -left-5 z-30 swiper-prev-btn'><PrevBtn/></div> */}
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={5}
-          modules={[Navigation , Autoplay]}
-          navigation={{ 
-            nextEl: ".swiper-next-btn",
-            prevEl: ".swiper-prev-btn",
-           }}
-          loop={true}
-          autoplay={{ 
-            delay: 2000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-        >
-          {
-            FeaturedProductData?.map((product)=>(
-              <SwiperSlide>
-              <ProductCards key={product.id} image={product?.image} rating={product?.rating} title={product.title} category={product.category} totalRating={product.totalRating} price={product.price} discount={product.discount} />
-              </SwiperSlide>
-            ))
-          }
-        </Swiper>
-      </div>
-    </>
-  )
+
+    const loadMore = () =>{
+        setinitialData((prev) => prev + 5)
+    }
+    return (
+        <>
+            <div className='relative mt-[80px]'>
+                <div className='flex items-center justify-between mb-[48px]'>
+                    <h2 className='font-poppins font-semibold text-[36px] text-black01 leading-[46px]'>{t('New_Products')}</h2>
+                    <div className='flex items-end gap-x-4'>
+                        <p className='font-montserrat font-normal text-base leading-6 text-black01'>{t('Sort_by')}</p>
+                        <MenubarCategories icons={true} className='font-montserrat font-bold text-base text-orange leading-[24px] cursor-pointer'/>
+                    </div>
+                </div>
+            </div>
+            <div className='grid grid-cols-5'>
+                {
+                    NewProductData?.slice(0,initialData)?.map((product)=>(
+                        <ProductCards key={product.id} image={product?.image} rating={product?.rating} title={product.title} category={product.category} totalRating={product.totalRating} price={product.price} discount={product.discount} />
+                    ))
+                }
+            </div>
+            {
+                initialData < NewProductData?.length && (
+                    <button className='py-4 px-10 border border-orange rounded-[10px] text-orange font-montserrat font-bold text-[20px] cursor-pointer mx-auto flex justify-center mt-[64px]' onClick={loadMore}>{t('Load_More')}</button>
+                )
+            }
+        </>
+    )
 }
 
 export default NewProduct
---
+++ src/pages/home/Components/NewProduct/NewProductData.js
@@ -0,0 +1,137 @@
+import ImageOne from '../../../../assets/ProductImage/featured_product/imageOne.webp'
+import Image2   from '../../../../assets/ProductImage/featured_product/imageTwo.webp'
+import Image3   from '../../../../assets/ProductImage/featured_product/image3.webp'
+import Image4   from '../../../../assets/ProductImage/featured_product/image4.webp'
+import Image5   from '../../../../assets/ProductImage/featured_product/image5.webp'
+import Image6   from '../../../../assets/ProductImage/featured_product/image6.webp'
+import Image7   from '../../../../assets/ProductImage/featured_product/image7.jpg'
+import Image8   from '../../../../assets/ProductImage/featured_product/image8.jpg'
+import Image9   from '../../../../assets/ProductImage/featured_product/image9.jpg'
+import Image10  from '../../../../assets/ProductImage/featured_product/image10.jpg'
+import Image11  from '../../../../assets/ProductImage/featured_product/image11.webp'
+import Image12  from '../../../../assets/ProductImage/featured_product/image12.webp'
+
+export const NewProductData = [
+  {
+    id: 1,
+    image: ImageOne,
+    category: "PHONE",
+    title: "JPhone 13 High Quality Value Buy Best Camera",
+    rating: 0,
+    totalRating: 0,
+    price: 999.0,
+    discount: 25,
+  },
+  {
+    id: 2,
+    image: Image2,
+    category: "LAPTOP",
+    title: "XBook Pro 15 Slim Performance Laptop",
+    rating: 0,
+    totalRating: 0,
+    price: 1299.0,
+    discount: 15,
+  },
+  {
+    id: 3,
+    image: Image3,
+    category: "HEADPHONE",
+    title: "SoundMax Wireless Noise Cancelling Headphone",
+    rating: 0,
+    totalRating: 0,
+    price: 199.0,
+    discount: 20,
+  },
+  {
+    id: 4,
+    image: Image4,
+    category: "WATCH",
+    title: "TimeX Smart Fitness Watch Waterproof",
+    rating: 0,
+    totalRating: 0,
+    price: 149.0,
+    discount: null,
+  },
+  {
+    id: 5,
+    image: Image5,
+    category: "TABLET",
+    title: "TabAir 11 Ultra Light Android Tablet",
+    rating: 0,
+    totalRating: 0,
+    price: 499.0,
+    discount: 18,
+  },
+  {
+    id: 6,
+    image: Image6,
+    category: "TV",
+    title: "VisionX 55 Inch 4K Ultra HD Smart TV",
+    rating: 0,
+    totalRating: 0,
+    price: 699.0,
+    discount: null,
+  },
+  {
+    id: 7,
+    image: Image7,
+    category: "CAMERA",
+    title: "CanonX Pro DSLR Professional Photography Camera",
+    rating: 0,
+    totalRating: 0,
+    price: 899.0,
+    discount: 30,
+  },
+  {
+    id: 8,
+    image: Image8,
+    category: "SPEAKER",
+    title: "BoomBox Bluetooth Party Speaker",
+    rating: 0,
+    totalRating: 0,
+    price: 249.0,
+    discount: null,
+  },
+  {
+    id: 9,
+    image: Image9,
+    category: "MONITOR",
+    title: "UltraView 27 Inch Curved Gaming Monitor",
+    rating: 0,
+    totalRating: 0,
+    price: 329.0,
+    discount: 17,
+  },
+  {
+    id: 10,
+    image: Image10,
+    category: "PRINTER",
+    title: "PrintJet Pro Wireless Inkjet Printer",
+    rating: 0,
+    totalRating: 0,
+    price: 159.0,
+    discount: null,
+  },
+  {
+    id: 11,
+    image: Image11,
+    category: "MOUSE",
+    title: "ClickPro Ergonomic Wireless Mouse",
+    rating: 0,
+    totalRating: 0,
+    price: 49.0,
+    discount: 5,
+  },
+  {
+    id: 12,
+    image: Image12,
+    category: "KEYBOARD",
+    title: "TypeFast Mechanical RGB Gaming Keyboard",
+    rating: 0,
+    totalRating: 0,
+    price: 119.0,
+    discount: null,
+  },
+];
+
+ 
