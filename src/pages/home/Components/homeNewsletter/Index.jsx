1fdc29ad58d11dad4e4b35c596823f64a3650beb
Md Mosharaf Hossain <parveznbh2026@gmail.com>
2025-10-25 00:26:47 +0600

    part-34-newsletter

--- src/pages/home/Components/homeNewsletter/Index.jsx
+++ src/pages/home/Components/homeNewsletter/Index.jsx
@@ -10,16 +10,16 @@ const HomeNewsletter = () => {
     <>
         <section className='mt-5'>
             <GlobalContainer>
-                <div className='rounded-[25px] grid grid-cols-[593px_1fr] overflow-hidden'>
-                    <div className='py-[114px] pl-[100px]  bg-linear-to-r from-white01 to-gray from-60%'>
-                        <h3 className='font-poppins font-semibold text-[36px] leading-[46px] text-black'>{ t("Get Our Updates") }</h3>
-                        <p className='font-montserrat font-normal text-[20px] leading-[30px] text-black01 mt-[15px] mb-[32px]'>{t("Browse our wide selection of electronics and find the perfect promo for you from newsletter.")}</p>
-                        <Input className=' !px-[18px] !py-[18px]  bg-white !mb-4' type="text" placeholder={t("Enter your email address ...")} />
+                <div className='lg:rounded-[25px] grid grid-cols-1 lg:grid-cols-[593px_1fr] overflow-hidden'>
+                    <div className='py-6 px-5 lg:py-[114px] lg:pl-[100px]  bg-linear-to-r from-white01 to-gray from-60%'>
+                        <h3 className='font-poppins font-semibold text-[26px] leading-7 lg:text-[36px] lg:leading-[46px] text-black'>{ t("Get Our Updates") }</h3>
+                        <p className='font-montserrat font-normal text-base lg:text-[20px] leading-[30px] text-black01 mt-[15px] mb-[32px]'>{t("Browse our wide selection of electronics and find the perfect promo for you from newsletter.")}</p>
+                        <Input className='!px-[18px] !py-[18px]  bg-white !mb-4' type="text" placeholder={t("Enter your email address ...")} />
                         <CommonButton className='!bg-orange !py-[20px] !px-10 !border-0 !rounded-[10px]'>
                             <span className='font-montserrat font-bold text-[20px] leading-[30px] text-white'>Subscribe</span>
                         </CommonButton>
                     </div>
-                    <div className='py-[114px] pr-[100px] bg-gray '>fsdf</div>
+                    <div className='py-[114px] pr-[100px] hidden lg:block bg-gray'></div>
                 </div>
             </GlobalContainer>
         </section>
